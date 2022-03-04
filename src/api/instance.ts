5197dd6b601aed2720c2f10bbc9d856fbe5dbca8
Manao-dev <pm@manao.by>
2022-03-04 16:26:10 +0300

    fix api

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const apiURL = process.env.NODE_ENV === "development"
-    ? "https://test-server-tau.vercel.app"
-    : "/bitrix/services/main/ajax.php"
+// const apiURL = process.env.NODE_ENV === "development"
+//     ? "https://test-server-tau.vercel.app"
+//     : "/bitrix/services/main/ajax.php"
 
-console.log(apiURL);
+const apiURL = "https://test-server-tau.vercel.app"
 
 export const fakeSessionId = "e14e316cb5cbcae4320a834ebb234f56";
 
