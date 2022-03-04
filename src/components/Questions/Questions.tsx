27cffa8d19d6e6144f2ea73c925c55240bc43582
Manao-dev <pm@manao.by>
2022-03-04 10:07:39 +0300

    fix lint

--- src/components/Questions/Questions.tsx
+++ src/components/Questions/Questions.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components'
 import { fakeSessionId } from '../../api/instance'
